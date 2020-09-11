bf510cc5743646f81e2cb18e972a1e22c7893e42
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-11 14:17:50 +0200

    Updates

---
+++ precache-manifest.efe1607c6532e7fed4e1440c429be69c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd9b2b72d97b269c8936c8f8f757218",
+    "url": "/reactJs-todo-hooks/index.html"
+  },
+  {
+    "revision": "57abb63ecd27e5c18430",
+    "url": "/reactJs-todo-hooks/static/css/main.958915ac.chunk.css"
+  },
+  {
+    "revision": "cfce65ee15800bc39038",
+    "url": "/reactJs-todo-hooks/static/js/2.66980d13.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/reactJs-todo-hooks/static/js/2.66980d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57abb63ecd27e5c18430",
+    "url": "/reactJs-todo-hooks/static/js/main.307e2dd9.chunk.js"
+  },
+  {
+    "revision": "c0e219a28adc4afd4fc4",
+    "url": "/reactJs-todo-hooks/static/js/runtime-main.65bb0bec.js"
+  }
+]);
